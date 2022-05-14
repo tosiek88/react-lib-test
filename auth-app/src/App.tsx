1d98d07baf53b026ba5803f6a4a5d2683ddbcfc6
Mateusz Tocha <Mateusz Tocha>
2022-05-14 18:35:02 +0000

    Fix dependencies

--- auth-app/src/App.tsx
+++ auth-app/src/App.tsx
@@ -1,14 +1,14 @@
-import React from "react";
+import { FC } from "react";
 import "./App.css";
 import { ContinuousSlider, HelloWorld } from "auth-comp-library";
 
-function App() {
+export const App: FC = () => {
   return (
     <div className="App">
       <HelloWorld />
-      <ContinuousSlider key={1}/>
+      <ContinuousSlider key={1} />
     </div>
   );
-}
+};
 
 export default App;
